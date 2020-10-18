6c655cc2da630014d33c259353627d4001d748e5
gilvan <g.r.almeida@live.com>
2020-10-18 18:49:45 -0300

    Obtendo elementos

--- Selenium.Tests/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
+++ Selenium.Tests/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
@@ -15,15 +15,28 @@ namespace Alura.LeilaoOnline.Selenium.Testes
         }
 
         [Fact]
-        public void Teste()
+        public void DadoInformacoesValidasDeveIrParaPaginaDeAgradecimento()
         {
             // Arrange - dado chrome aberto, página inicial do sist. de leilões,
             // dados de registros válidos informados
             driver.Navigate().GoToUrl("http://localhost:5000");
 
+            IWebElement inputNome = driver.FindElement(By.Id("Nome"));
+            IWebElement inputEmail = driver.FindElement(By.Id("Email"));
+            IWebElement inputSenha = driver.FindElement(By.Id("Password"));
+            IWebElement inputConfirmacaoSenha = driver.FindElement(By.Id("ConfirmPassword"));
+            IWebElement btnRegistro = driver.FindElement(By.Id("btnRegistro"));
+
+            inputNome.SendKeys("Daniel Portugal");
+            inputEmail.SendKeys("daniel.portugal@caelum.com.br");
+            inputSenha.SendKeys("123");
+            inputConfirmacaoSenha.SendKeys("123");
+
             // Act - efetuo o registro
+            btnRegistro.Click();
 
             // Assert - devo ser direcionado para uma página de agradecimento
+            Assert.Contains("Obrigado por se registrar!", driver.PageSource);
         }
     }
 }
