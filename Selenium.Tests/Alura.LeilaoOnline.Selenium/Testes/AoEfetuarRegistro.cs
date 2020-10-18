48af138497cba4d2adb7a133ed09e8190ba3e8ec
gilvan <g.r.almeida@live.com>
2020-10-18 19:11:40 -0300

    Registro inválido

--- Selenium.Tests/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
+++ Selenium.Tests/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
@@ -38,5 +38,37 @@ namespace Alura.LeilaoOnline.Selenium.Testes
             // Assert - devo ser direcionado para uma página de agradecimento
             Assert.Contains("Obrigado por se registrar!", driver.PageSource);
         }
+
+        [Theory]
+        [InlineData("", "daniel.portugal@caelum.com.br", "123", "123")]
+        [InlineData("Daniel Portugal", "daniel", "123", "123")]
+        [InlineData("Daniel Portugal", "daniel.portugal@caelum.com.br", "123", "456")]
+        [InlineData("Daniel Portugal", "daniel.portugal@caelum.com.br", "123", "")]
+        public void DadoInformacoesInvalidasDeveContinuarNaHome(
+            string nome,
+            string email,
+            string senha,
+            string confirmacaoSenha)
+        {
+            // Arrange
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            IWebElement inputNome = driver.FindElement(By.Id("Nome"));
+            IWebElement inputEmail = driver.FindElement(By.Id("Email"));
+            IWebElement inputSenha = driver.FindElement(By.Id("Password"));
+            IWebElement inputConfirmacaoSenha = driver.FindElement(By.Id("ConfirmPassword"));
+            IWebElement btnRegistro = driver.FindElement(By.Id("btnRegistro"));
+
+            inputNome.SendKeys(nome);
+            inputEmail.SendKeys(email);
+            inputSenha.SendKeys(senha);
+            inputConfirmacaoSenha.SendKeys(confirmacaoSenha);
+
+            // Act
+            btnRegistro.Click();
+
+            // Assert
+            Assert.Contains("section-registro", driver.PageSource);
+        }
     }
 }
