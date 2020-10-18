6bb61d8074b56ceafdaca3d9b1d806889de878bb
gilvan <g.r.almeida@live.com>
2020-10-18 16:23:44 -0300

    Testando o registro

---
+++ Selenium.Tests/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
@@ -0,0 +1,29 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarRegistro
+    {
+        private IWebDriver driver;
+
+        public AoEfetuarRegistro(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void Teste()
+        {
+            // Arrange - dado chrome aberto, página inicial do sist. de leilões,
+            // dados de registros válidos informados
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
+            // Act - efetuo o registro
+
+            // Assert - devo ser direcionado para uma página de agradecimento
+        }
+    }
+}
